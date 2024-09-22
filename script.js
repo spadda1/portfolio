28a7baf5edba0fe517ba25427158ef7017e869f5
Sharan Padda <sharanpadda2003@gmail.com>
2024-09-21 17:46:23 -0700

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Aspiring Software Engineer", "Software Engineering Fellow", "IT Support Specialist", "CSE Undergraduate Student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
